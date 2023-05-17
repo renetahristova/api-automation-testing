94f3f74db3c7e86841436e5707ca291048a9b857
reneta <renetaghristova@gmail.com>
2023-05-17 11:38:55 +0300

    get users/:id test

--- test/users.cjs
+++ test/users.cjs
@@ -1,15 +1,27 @@
 const request = require('supertest');
+const chai = require('chai');
 require("dotenv").config();
 
+const expect = chai.expect;
 const TOKEN = process.env.USER_TOKEN;
+
 const url = 'https://gorest.co.in/public/v2/';
 
 describe('Users', () => {
     it('GET /users', () => {
-        request(url)
-            .get(`users?access-token=${TOKEN}`).end((err,res) => {
-                console.log(res.body)
-                console.log(err);
+   
+        return request(url)
+            .get(`users?access-token=${TOKEN}`).then((res) => {
+                expect(res.body.data).to.not.be.null;
             });
     });
+
+    it('GET /users/:id', () => {
+        return request(url)
+        .get(`users/1132?access-token=${TOKEN}`).then((res) => {
+           expect(res.body.id).to.be.eql(1132);
+          // expect(res.body).to.be.an('object');
+          //console.log(res.body)
+        });
+    })
 });
